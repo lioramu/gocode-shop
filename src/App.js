26dc7c66eed8e177d3689a0727a7b54473ce1ae0
liora muhailov <lioraam87@gmail.com>
2022-06-11 20:04:20 +0300

    targil29

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
-import ProductsList from './Data';
-import { useState } from "react";
+//import ProductsList from './Data';
+import { useEffect, useState } from "react";  
 
 function App() {
-const [products,setProducts]= useState(ProductsList);
+const [products,setProducts]= useState([]);
+const [listProducts,setlistProducts]=useState([]);
 
-const categories = ProductsList.map(p => p.category)
+useEffect(()=>{
+   fetch("https://fakestoreapi.com/products")
+.then((res)=>res.json())
+.then((list)=> {setProducts(list); setlistProducts (list)});
+},[])
+
+const categories = listProducts.map(p => p.category)
   .filter((value, index, array) => array.indexOf(value)===index);
 
 function filterProducts (category) {
- setProducts(ProductsList.filter((product)=>
-   category==="all" ? product : product.category ===category))
+  setProducts(listProducts.filter((product)=>
+   category==="all" ? product : product.category ===category));
+  
   }
   return (
+
       <>
-      <Header categories={categories} filterProducts={filterProducts} /> 
+      <Header 
+       categories={categories} 
+       filterProducts={filterProducts}
+       /> 
       <Products product={products}/> 
       </>
   );
 };
 export default App;
+
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,11 +6,15 @@ const Products = ({product}) => {
     
     return ( 
     <section className="products">
- {
+ {product.length > 0 ?
  product.map(({id, price, title, image})=>{
 
-return  <Product key={id} title={title} price={price} image={image} />;
- })}         
+return <Product key={id} title={title} price={price} image={image} />;
+ }) : <div class="spinner-examples">
+ <div class="example">
+   <span class="smooth spinner" />
+ </div>
+</div> }  
   </section>
   );
  };
